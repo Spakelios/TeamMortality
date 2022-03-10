ee74ce86ab58b68f2207fb7ffd46e6750516714d
Spakelios <73300254+Spakelios@users.noreply.github.com>
2022-03-10 16:18:03 +0000

    he nyoom

---
+++ ConstellationConfrontation1/Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+    private Vector2 targetPosition;
+
+    void Start()
+    {
+
+        targetPosition = new Vector2(5f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetPosition = Input.mousePosition;
+            targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(targetPosition.x, targetPosition.y, 0.0f));
+        }
+
+        this.transform.position = Vector2.MoveTowards(this.transform.position, targetPosition, speed * Time.deltaTime);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Eye"))
+        {
+            gameObject.GetComponent<Animator>().Play("Eye fixed");
+        }
+    }
+}
