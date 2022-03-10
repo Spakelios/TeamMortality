59c9d1481c49c2e598c2d84023bb75a4727c3363
Spakelios <73300254+Spakelios@users.noreply.github.com>
2022-03-10 18:12:40 +0000

    started making inventory

---
+++ ConstellationConfrontation1/Assets/pickUp.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUp : MonoBehaviour {
+
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) {
+            // spawn the sun button at the first available inventory slot ! 
+            
+
+            for (int i = 0; i < inventory.items.Length; i++)
+            {
+                if (inventory.items[i] == 0) { // check whether the slot is EMPTY
+                    inventory.items[i] = 1; // makes sure that the slot is now considered FULL
+                    Instantiate(itemButton, inventory.slots[i].transform, false); // spawn the button so that the player can interact with it
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+        
+    }
+}
