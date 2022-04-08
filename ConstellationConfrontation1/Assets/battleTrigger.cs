e192895fd5db7e7dd506c3189929fd12c56da9c1
AoifeBerry <71941986+AoifeBerry@users.noreply.github.com>
2022-04-08 14:48:32 +0100

    small changes

---
+++ ConstellationConfrontation1/Assets/battleTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class battleTrigger : MonoBehaviour
+{
+    public GameObject Box;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("b"))
+        {
+            Box.SetActive(true);
+        }
+    }
+
+}
