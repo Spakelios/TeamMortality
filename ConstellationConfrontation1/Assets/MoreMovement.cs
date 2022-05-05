8354703b6a4760b0fb8e5e7948c032b092449d9f
Spakelios <73300254+Spakelios@users.noreply.github.com>
2022-05-05 23:03:58 +0100

    oop

---
+++ ConstellationConfrontation1/Assets/MoreMovement.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreMovement : MonoBehaviour
+{
+    public Rigidbody2D playerRB;
+    private Vector2 movement;
+    public float movementSpeed;
+    public bool canMove;
+    
+    private void Update()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+        playerRB.MovePosition(playerRB.position + movement * (movementSpeed * Time.fixedDeltaTime));
+    }
+}
