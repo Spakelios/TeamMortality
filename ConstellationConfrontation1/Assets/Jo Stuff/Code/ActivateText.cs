5da6891c5e98bb5173b7607319b6c0244696df32
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2022-05-06 02:35:45 +0100

    Ice Planet Quest Line Completed

--- ConstellationConfrontation1/Assets/Jo Stuff/Code/ActivateText.cs
+++ ConstellationConfrontation1/Assets/Jo Stuff/Code/ActivateText.cs
@@ -7,6 +7,7 @@ using TMPro;
 public class ActivateText : MonoBehaviour
 {
     public TextAsset theText;
+    public TextAsset questLine;
 
     public GameObject interactKey;
 
@@ -19,6 +20,10 @@ public class ActivateText : MonoBehaviour
     private bool waitForPress;
 
     public bool destroyWhenActivated;
+    public MissionDialogue missionDialogue;
+
+    public bool questAdvanced;
+    
     
 
     
@@ -27,6 +32,7 @@ public class ActivateText : MonoBehaviour
     {
         theTextBox = FindObjectOfType<TextboxManager>();
         interactKey.SetActive(false);
+        questAdvanced = false;
 
 
 
@@ -36,10 +42,12 @@ public class ActivateText : MonoBehaviour
     {
         if (waitForPress && Input.GetKeyDown(KeyCode.Return))
         {
-            theTextBox.ReloadScript(theText);
-            theTextBox.currentLine = startLine;
-            theTextBox.endAtLine = endLine;
-            theTextBox.EnableTextBox();
+
+                theTextBox.ReloadScript(theText);
+                theTextBox.currentLine = startLine;
+                theTextBox.endAtLine = endLine;
+                theTextBox.EnableTextBox();
+            
 
 
             if (destroyWhenActivated)
@@ -69,10 +77,21 @@ public class ActivateText : MonoBehaviour
                     return;
                 }
                 
-                theTextBox.ReloadScript(theText);
-                theTextBox.currentLine = startLine;
-                theTextBox.endAtLine = endLine;
-                theTextBox.EnableTextBox();
+                if (missionDialogue.acceptedQuest)
+                {
+                    questAdvanced = true;
+                    theTextBox.ReloadScript(questLine);
+                    theTextBox.currentLine = startLine;
+                    theTextBox.endAtLine = endLine;
+                    theTextBox.EnableTextBox();
+                }
+                else
+                {
+                    theTextBox.ReloadScript(theText);
+                    theTextBox.currentLine = startLine;
+                    theTextBox.endAtLine = endLine;
+                    theTextBox.EnableTextBox();
+                }
             }
         }
     }
---
+++ ConstellationConfrontation1/Assets/Jo Stuff/Code/MissionManager.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MissionManager : MonoBehaviour
+{
+    public GameObject textBox;
+    public TextMeshProUGUI theText;
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+    
+    public KeyboardPlayerMovement player;
+
+    public bool isActive;
+    public bool stopPlayerMovement;
+    public GameObject firstChoice;
+    public GameObject secondChoice;
+    
+    public MissionDialogue missionDialogue;
+    private void Start()
+    {
+        player = FindObjectOfType<KeyboardPlayerMovement>();
+
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+
+        if (isActive)
+        {
+            EnableTextBox();
+        }
+        else
+        {
+            DisableTextBox();
+        }
+    }
+
+    private void Update()
+    {
+
+        if (!isActive)
+        {
+            return;
+        }
+        
+        theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Space) && currentLine < 6)
+        {
+            currentLine += 1;
+        }
+
+        if (currentLine > endAtLine)
+        {
+            DisableTextBox();
+
+            if (!missionDialogue.choiceMade)
+            {
+                ChoiceButtons();
+            }
+            
+            else if (missionDialogue.choiceMade && !missionDialogue.secondChoiceMade)
+            {
+                ChoiceButtons2();
+            }
+        }
+    }
+
+    public void EnableTextBox()
+    {
+        textBox.SetActive(true);
+        isActive = true;
+
+        if (stopPlayerMovement)
+        {
+            player.canMove = false;
+        }
+    }
+
+    private void DisableTextBox()
+    {
+        textBox.SetActive(false);
+        
+        
+        isActive = false;
+        player.canMove = true;
+        
+    }
+
+    public void ReloadScript(TextAsset theText)
+    {
+        if (theText != null)
+        {
+            textLines = new string[1];
+            textLines = (theText.text.Split('\n'));
+        }
+    }
+
+    private void ChoiceButtons()
+    {
+        firstChoice.SetActive(true);
+        
+        isActive = true;
+        player.canMove = false;
+    }
+
+    private void ChoiceButtons2()
+    {
+        secondChoice.SetActive(true);
+        isActive = true;
+        player.canMove = false;
+    }
+}
